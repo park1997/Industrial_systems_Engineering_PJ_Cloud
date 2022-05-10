52ab0bdd287528249890c9f62bc348f705a46f8a
park1997 <iambyunghyun97@gmail.com>
2022-05-11 03:21:49 +0900

    Update server.py

--- server.py
+++ server.py
@@ -2,9 +2,9 @@
 import uvicorn
 
 if __name__=="__main__":
-    uvicorn.run("app.main:app",host="localhost",port=8000,reload=True)
+    # uvicorn.run("app.main:app",host="localhost",port=8000,reload=True)
     # uvicorn.run("app.main:app",host="localhost",port=8000)
-    # uvicorn.run("app.main:app",host="0.0.0.0",port=80,reload=False)
+    uvicorn.run("app.main:app",host="0.0.0.0",port=80,reload=False)
 
 
 
